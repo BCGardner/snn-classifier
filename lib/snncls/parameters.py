42879321229bc8b4f20eea7651c4c5f7845df786
Brian Gardner <b.gardner@surrey.ac.uk>
2019-01-12 01:23:57 +0000

    add: rate penalty term

--- lib/snncls/parameters.py
+++ lib/snncls/parameters.py
@@ -13,11 +13,19 @@ class ParamSet(dict):
     """
     def overwrite(self, **D):
         """
-        Update only matching keys with type safety.
+        Update only matching keys with type safety. None value can be
+        overwritten by a float value.
         """
         for k, v in D.items():
             if k in self:
-                if isinstance(v, type(self[k])):
+                # If new value has float or None type, then update
+                if self[k] is None:
+                    if isinstance(v, float) or v is None:
+                        self[k] = v
+                    else:
+                        raise TypeError
+                # If new value matches current value type, then update
+                elif isinstance(v, type(self[k])):
                     self[k] = v
                 else:
                     raise TypeError
