d538b7ffdee830f5a685682be32ee3c411bcfed3
Brian Gardner <b.gardner@surrey.ac.uk>
2019-08-16 18:18:13 +0100

    WIP: nrn class response func

--- lib/snncls/neuron/models.py
+++ lib/snncls/neuron/models.py
@@ -12,7 +12,9 @@ from __future__ import division
 
 import numpy as np
 
+from .escape_noise import ExpRate
 from ..parameters import ParamSet
+from ..preprocess import pattern2psps
 
 
 class SRM(object):
@@ -21,6 +23,10 @@ class SRM(object):
 
     Parameters
     ----------
+    stochastic : bool, optional, default False
+        Enable stochastic spike generator.
+    EscapeRate : class, default ExpRate
+        Escape noise model used to distribute spikes, if stochastic enabled.
     tau_m : float
         Membrane time constant (ms).
     tau_s : float
@@ -34,7 +40,9 @@ class SRM(object):
     q : float
         Charge transferred due to a presynaptic spike (nA).
     """
-    def __init__(self, **kwargs):
+    def __init__(self, stochastic=False, EscapeRate=ExpRate, **kwargs):
+        # Stochastic spike generator
+        self.stochastic = stochastic
         # Default cell prms
         self.prms = ParamSet({'tau_m': 10.,
                               'tau_s': 5.,
@@ -42,11 +50,40 @@ class SRM(object):
                               'cm': 2.5,
                               'kappa_0': -15.,
                               'q': 5.})
-        self.update(**kwargs)
+        self.update(EscapeRate=EscapeRate, **kwargs)
 
-    def update(self, **kwargs):
+    def update(self, EscapeRate=ExpRate, **kwargs):
         self.prms.overwrite(**kwargs)
         # Derived values
         # psp coeff (mV)
         self.prms['psp_coeff'] = self.prms['q'] / self.prms['cm'] * \
             self.prms['tau_m'] / (self.prms['tau_m'] - self.prms['tau_s'])
+        # Stochastic spike generator
+        if self.stochastic:
+            self.esc_rate = EscapeRate(theta=self.prms['theta'], **kwargs)
+
+    def response(self, stimulus, w, dt, duration):
+        """
+        Neuron's response to a driving stimulus, through a weight vector.
+
+        Parameters
+        ----------
+        stimulus : list, len (num_inputs)
+            Set of input spike trains.
+        w : array, shape (num_inputs,)
+            Input weight vector.
+        dt : float
+            Time resolution.
+        duration : float
+            Cut-off point for determining neuron response.
+        """
+        # Transform stimulus to PSPs for this neuron
+        # psps : array, shape (num_inputs, num_iter)
+        psps = pattern2psps(stimulus, dt, duration, **self.prms)
+        spike_train = np.array([])
+
+        # State variable updates
+        potentials = np.dot(w, psps)
+        # Determine spikes
+        if self.stochastic:
+            unif_samples = self.rng.uniform(size=len(potentials))
